f5128ddffabd7f8ba048547431b513269d24cb59
ilya <druzilya@ukr.net>
2022-12-27 01:36:51 +0200

    Add element for work in element_id.py

---
+++ framework/element_id.py
@@ -0,0 +1,12 @@
+class LoginPageElements:
+    BUTTON_LOGIN = "com.ajaxsystems:id/login"
+    BUTTON_CREATE_ACCOUNT = "com.ajaxsystems:id/registration"
+    BUTTON_FORGOT_PASSWORD = "com.ajaxsystems:id/forgot"
+    BUTTON_NEXT = "com.ajaxsystems:id/next"
+    INPUT_EMAIL = "com.ajaxsystems:id/login"
+    INPUT_PASSWORD = "com.ajaxsystems:id/password"
+
+
+class HomePageElements:
+    BUTTON_ADD_HUB = "com.ajaxsystems:id/hubAdd"
+    BUTTON_SIDEBAR = "com.ajaxsystems:id/menuDrawer"
